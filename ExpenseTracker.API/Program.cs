2724c4dba74a9632c1d6c60e3be1df4c54bd395f
Sean Schwabenlander <sean@schwabenlander.com>
2021-12-17 19:57:59 -0600

    Added a missing API end point for getting a specific expense (by ID)

--- ExpenseTracker.API/Program.cs
+++ ExpenseTracker.API/Program.cs
@@ -36,48 +36,63 @@ app.UseHttpsRedirection();
 app.UseCors("CorsPolicy");
 
 app.MapGet("/api/expenses", async (ExpenseDb db) => 
-    await db.ExpenseItems.ToListAsync());
+    await db.ExpenseItems.ToListAsync())
+    .WithName("GetAllExpenses");
+
+app.MapGet("/api/expenses/{id:int}", async (int id, ExpenseDb db) =>
+    {
+        var result = await db.ExpenseItems.FindAsync(id);
+        if (result is null)
+            return Results.NotFound();
+        else
+            return Results.Ok(result);
+    })
+    .WithName("GetExpenseById");
 
 app.MapGet("/api/expenses/year/{year:int}", async (int year, ExpenseDb db) =>
-    await db.ExpenseItems.Where(expense => expense.Date.Year == year).ToListAsync());
+    await db.ExpenseItems.Where(expense => expense.Date.Year == year).ToListAsync())
+    .WithName("GetExpensesByYear");
 
 app.MapPost("/api/expenses", async (ExpenseItem expense, ExpenseDb db) =>
-{
-    expense.Id = default(int);
-    expense.Date = expense.Date.Date;
-    db.ExpenseItems.Add(expense);
-    await db.SaveChangesAsync();
+    {
+        expense.Id = default(int);
+        expense.Date = expense.Date.Date;
+        db.ExpenseItems.Add(expense);
+        await db.SaveChangesAsync();
 
-    return Results.Created($"/expenses/{expense.Id}", expense);
-});
+        return Results.CreatedAtRoute("GetExpenseById", new { id = expense.Id }, expense);
+    })
+    .WithName("PostExpense");
 
 app.MapPut("/api/expenses/{id:int}", async (int id, ExpenseItem expense, ExpenseDb db) => 
-{ 
-    var savedExpense = await db.ExpenseItems.FindAsync(id);
+    { 
+        var savedExpense = await db.ExpenseItems.FindAsync(id);
 
-    if (savedExpense is null)
-        return Results.NotFound();
+        if (savedExpense is null)
+            return Results.NotFound();
 
-    savedExpense.Title = expense.Title;
-    savedExpense.Amount = expense.Amount;
-    savedExpense.Date = expense.Date.Date;
+        savedExpense.Title = expense.Title;
+        savedExpense.Amount = expense.Amount;
+        savedExpense.Date = expense.Date.Date;
 
-    await db.SaveChangesAsync();
+        await db.SaveChangesAsync();
 
-    return Results.NoContent();
-});
+        return Results.NoContent();
+    })
+    .WithName("PutExpense");
 
 app.MapDelete("/api/expenses/{id:int}", async (int id, ExpenseDb db) =>
-{
-    if (await db.ExpenseItems.FindAsync(id) is ExpenseItem expense)
     {
-        db.ExpenseItems.Remove(expense);
-        await db.SaveChangesAsync();
-        return Results.Ok(expense);
-    }
+        if (await db.ExpenseItems.FindAsync(id) is ExpenseItem expense)
+        {
+            db.ExpenseItems.Remove(expense);
+            await db.SaveChangesAsync();
+            return Results.Ok(expense);
+        }
 
-    return Results.NotFound();
-});
+        return Results.NotFound();
+    })
+    .WithName("DeleteExpense");
 
 app.Run();
 
